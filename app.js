d423f5b6c982cc5bdb850386c1166043566a8f3f
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-24 09:52:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ code = code.replace(/src="\//gi, 'src="http://carbon-proxy.herokuapp.com/proxy?u
 //code = code.replace(/src=".\//gi, 'src="http://carbon-proxy.herokuapp.com/proxy?url=' + domain + '/');
 //code = code.replace(/url\("\//gi, 'url("http://carbon-proxy.herokuapp.com/proxy?url=' + url2 + '/');
 //code = code.replace(/url\(".\//gi, 'url("http://carbon-proxy.herokuapp.com/proxy?url=' + domain + '/');
-            res.send(code);
+            res.send(url2);
 return;
     });
 });
