f5431ddd8dba77fc4337a50f88a31b14b5206dd7
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-24 20:20:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -47,12 +47,10 @@ res.setHeader("content-type", "text/html");
 res.setHeader("content-type", "text/" + type);
     }
 }
-    url2 = url2.split("?", 1);
-    url2 = url2.toString();
 code = code.replace(/href=".\//gi, 'href="http://carbon-proxy.herokuapp.com/proxy?url=' + domain + '/');
 code = code.replace(/href="(?!https:\/\/)/gi, 'href="http://carbon-proxy.herokuapp.com/proxy?url=' + url2 + '/');
 code = code.replace(/href="\//gi, 'href="http://carbon-proxy.herokuapp.com/proxy?url=' + url2 + '/');
-code = code.replace(/a href="http\s\:\/\/www./gi, 'a href="http://carbon-proxy.herokuapp.com/proxy?url=');
+code = code.replace(/a href="https\:\/\/www./gi, 'a href="http://carbon-proxy.herokuapp.com/proxy?url=');
 code = code.replace(/src="\//gi, 'src="http://carbon-proxy.herokuapp.com/proxy?url=' + url2 + '/');
 //code = code.replace(/src=".\//gi, 'src="http://carbon-proxy.herokuapp.com/proxy?url=' + domain + '/');
 //code = code.replace(/url\("\//gi, 'url("http://carbon-proxy.herokuapp.com/proxy?url=' + url2 + '/');
