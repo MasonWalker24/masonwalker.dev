2ad605da0c41374d595411e68aeda01487fb98b1
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-25 16:04:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let path2;
 app.get('/proxy', function(req, res){
     window.open("./" + path2,"_self");
 path2 = req.qeury.url;
-)}
+
 app.get('/proxy/' + path2, function(req, res){
     url = req.query.url;
     url2 = req.query.url;
