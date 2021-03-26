5e281aa37c6ad7c3eb12c9d5cc1c744dcde46ee9
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-26 17:41:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ var type = str[str.length - 1];
     if(type == "png")
     {
         res.setHeader("content-type", "image/png");
-        fs.writeFile(url, code, Callback);
+        fs.writeFile(url, code);
     } else{
     if(type == "svg")
     {
