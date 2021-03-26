4b5f0e9e8f104e34b9bd55b94b6cc7930147740c
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-26 09:56:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ url = "https://" + url;
     let count = 0;
     for(let i = 0; i <= num - 1; i++)
     {
-     path2 = test[i];
+     path2 = path + test[i];
     }
     
 fetch(url).then(function (res) {
