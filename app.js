8e086ccfae3a5bf7f9ef1ef51750a0e312a7ce7b
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-25 20:52:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const querystring = require('querystring');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -59,7 +60,8 @@ code = code.replace(/src="\//gi, 'src="http://carbon-proxy.herokuapp.com/proxy?u
 //code = code.replace(/src=".\//gi, 'src="http://carbon-proxy.herokuapp.com/proxy?url=' + domain + '/');
 code = code.replace(/url\("\//gi, 'url("http://carbon-proxy.herokuapp.com/proxy?url=' + url2 + '/');
 //code = code.replace(/url\(".\//gi, 'url("http://carbon-proxy.herokuapp.com/proxy?url=' + domain + '/');
-            res.send(code);
+    test = querystring.parse(test);        
+    res.send(test);
 return;
     });
 });
