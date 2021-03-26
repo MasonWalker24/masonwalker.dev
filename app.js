453db24c1f20ea5f5c15d997bea48fcb6c79ccdc
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-26 17:34:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const http = require('http');
 const https = require('https');
+const fs = require('fs');
 
 let url;
 let code;
@@ -62,7 +63,7 @@ url = url.split("?", 1);
     url = url.toString();
 var str = url.split("."); 
 var type = str[str.length - 1];
-const fs = require(fs);
+
     if(type == "png")
     {
         res.setHeader("content-type", "image/png");
