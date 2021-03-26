f26264e2b19899f56a19fffcca7bee34dd5e69be
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-26 12:03:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ res.setHeader("content-type", "text/html");
     } 
     }
     }
-code = code.replace(/href=".\//gi, 'href="http://carbon-proxy.herokuapp.com/proxy?url=' + domain + '/');
+code = code.replace(/href=".\//gi, 'href="http://carbon-proxy.herokuapp.com/proxy?url=' + test + '/');
 code = code.replace(/href="(?!https:\/\/|\/)/gi, 'href="http://carbon-proxy.herokuapp.com/proxy?url=' + url2 + '/');
 code = code.replace(/href="\//gi, 'href="http://carbon-proxy.herokuapp.com/proxy?url=' + domain + '/');
 code = code.replace(/content="\//gi, 'content="http://carbon-proxy.herokuapp.com/proxy?url=' + url2 + '/');
