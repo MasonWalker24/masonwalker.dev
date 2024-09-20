5084224351e3e58d26d90d7b312e11a3b08fe767
Mason Walker <46076583+MasonWalker24@users.noreply.github.com>
2024-09-20 13:30:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,55 @@ const router = express.Router();
 app.get('/', function(req, res){
 res.sendFile('index.html', { root: __dirname + "/public" } );
 });
+//CarbonProxy Stuff
 
+const path = require('path');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+let url;
+let code;
+let domain;
+let find1;
+let num;
+let test;
+let url2;
+
+app.get('/proxy', function(req, res){
+    
+url = req.query.url;
+url2 = req.query.url;
+num = url.indexOf("/");
+domain = url.split('/')[0];
+url = "https://" + url;
+find1 = /(href)="./gi;
+
+fetch(url).then(function (res) {
+        return res.text();
+    }).then(function (text) {
+code = text;
+var str = url.split("."); 
+var type = str[str.length - 1];
+if(domain == url2) {
+res.setHeader("content-type", "text/html");
+} else {
+res.setHeader("content-type", "text/" + type);
+}
+code = code.replace(/href=".\//gi, 'href="http://localhost/proxy?url=' + domain + '/');
+code = code.//replace(/href="/gi, 'href="http://localhost/proxy?url=' + url2 + '/');
+code = code.replace(/href="\//gi, 'href="http://localhost/proxy?url=' + url2 + '/');
+code = code.replace(/src="\//gi, 'src="http://localhost/proxy?url=' + url2 + '/');
+code = code.replace(/src=".\//gi, 'src="http://localhost/proxy?url=' + domain + '/');
+code = code.replace(/url\("\//gi, 'url("http://localhost/proxy?url=' + url2 + '/');
+code = code.replace(/url\(".\//gi, 'url("http://localhost/proxy?url=' + domain + '/');
+            res.send(code);
+return;
+    });
+});
+
+//End CarbonProxy Stuff
 app.use(express.static('public'))
 app.use('/', router);
 const PORT = process.env.PORT || 8080;
